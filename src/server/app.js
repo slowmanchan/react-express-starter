f0fe5daa7c693788067e710f10efe85922be084d
Norman Chan <slowmanchan@gmail.com>
2018-04-05 19:50:14 -0400

    added gitignore

--- src/server/app.js
+++ src/server/app.js
@@ -5,11 +5,13 @@ import React from 'react'
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, './server/')));
+const dist = path.join(__dirname, '../../dist')
+app.use(express.static(dist));
 
+console.log()
 app.get('/*', (req, res) => {
   res.status(200)
-  res.sendFile(path.join(__dirname, './views/index.html')
+  res.sendFile(path.join(dist, 'index.html'))
 })
 
 // Opens a socket and listens for connections only if there is no parent module running the script.
